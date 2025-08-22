ddd53eefcd06557bc60c581e272fcb9f2b08336a
ana <163579361+euanatxt@users.noreply.github.com>
2025-08-22 06:19:09 -0300

    Delete src directory

--- src/main/java/com/analuiza/tarefasapi/repository/TarefaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.analuiza.tarefasapi.repository;
-
-import com.analuiza.tarefasapi.model.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
